57e8874bf111f1527947335025701e9e16ac9567
Oleg Andreych <kjiec4@gmail.com>
2017-10-04 18:30:39 +0500

    Calendar initialization before start.

--- src/main/kotlin/org/andreych/russiancalendar/updater/CalendarDataUpdater.kt
+++ src/main/kotlin/org/andreych/russiancalendar/updater/CalendarDataUpdater.kt
@@ -7,6 +7,7 @@ import org.apache.commons.lang3.time.DateUtils
 import org.slf4j.LoggerFactory
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Service
+import javax.annotation.PostConstruct
 
 /**
  * Сервис обновления данных производственного календаря.
@@ -18,6 +19,11 @@ class CalendarDataUpdater(private val datasource: CalendarDatasource,
 
     companion object {
         val LOG = LoggerFactory.getLogger(CalendarDataUpdater::class.java)
+    }
+
+    @PostConstruct
+    fun init() {
+        update()
     }
 
     @Scheduled(fixedRate = DateUtils.MILLIS_PER_DAY)
