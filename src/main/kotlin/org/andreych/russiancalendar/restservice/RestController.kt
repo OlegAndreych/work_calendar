dd1c2912ca3b24e8cc320c7e2d686a16fb09fbb0
Oleg Andreych <kjiec4@gmail.com>
2017-10-04 19:27:16 +0500

    Response headers are now statically pregenerated.

--- src/main/kotlin/org/andreych/russiancalendar/restservice/RestController.kt
+++ src/main/kotlin/org/andreych/russiancalendar/restservice/RestController.kt
@@ -12,16 +12,20 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 class RestController(private val calendarStorage: CalendarStorage) {
 
+    companion object {
+        private val headers = HttpHeaders()
+
+        init {
+            headers.add("Cache-Control", "no-cache, no-store, must-revalidate")
+            headers.add("Pragma", "no-cache")
+            headers.add("Expires", "0")
+            headers.add("Content-Encoding", "gzip")
+            headers.add("Content-Disposition", "attachment; filename=\"calendar.ics\"")
+        }
+    }
+
     @GetMapping("/")
     fun workCalendar(): ResponseEntity<ByteArrayResource> {
-
-        val headers = HttpHeaders()
-        headers.add("Cache-Control", "no-cache, no-store, must-revalidate")
-        headers.add("Pragma", "no-cache")
-        headers.add("Expires", "0")
-        headers.add("Content-Encoding", "gzip")
-        headers.add("Content-Disposition", "attachment; filename=\"calendar.ics\"")
-
         val bytes = calendarStorage.getBytes()
 
         return if (bytes != null) {
