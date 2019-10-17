e5373415ea5340e86beaff28d288c2682cb065ce
Oleg Andreych <kjiec4@gmail.com>
2019-10-17 23:21:33 +0500

    * Added a dependencies versions updates report plugin; * Bumped dependencies versions;

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,8 +3,9 @@ import org.gradle.api.JavaVersion.VERSION_11
 plugins {
     application
     kotlin("jvm") version "1.3.50"
-    id("org.springframework.boot").version("2.1.8.RELEASE")
+    id("org.springframework.boot").version("2.2.0.RELEASE")
     id("org.jetbrains.kotlin.plugin.spring").version("1.3.50")
+    id("com.github.ben-manes.versions").version("0.27.0")
 }
 
 application {
@@ -14,6 +15,16 @@ application {
 allprojects {
     group = "org.andreych"
     version = "1.0-SNAPSHOT"
+}
+
+buildscript {
+    repositories {
+        jcenter()
+    }
+
+    dependencies {
+        classpath("com.github.ben-manes:gradle-versions-plugin:0.27.0")
+    }
 }
 
 repositories {
@@ -34,28 +45,28 @@ kotlin {
 
 dependencies {
     compile("org.jetbrains.kotlin:kotlin-stdlib:1.3.50")
-    compile("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.1")
+    compile("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.2")
     compile("org.jetbrains.kotlin:kotlin-reflect:1.3.50")
-    compile("com.fasterxml.jackson.module:jackson-module-kotlin:2.10.0.pr3")
-    compile("org.mnode.ical4j:ical4j:3.0.10")
+    compile("com.fasterxml.jackson.module:jackson-module-kotlin:2.10.0")
+    compile("org.mnode.ical4j:ical4j:3.0.11")
 
-    compile("org.springframework.boot:spring-boot-devtools:2.1.8.RELEASE")
-    compile("org.springframework.boot:spring-boot-starter-jdbc:2.1.8.RELEASE")
-    compile("org.springframework.boot:spring-boot-starter-web:2.1.8.RELEASE") {
+    compile("org.springframework.boot:spring-boot-devtools:2.2.0.RELEASE")
+    compile("org.springframework.boot:spring-boot-starter-jdbc:2.2.0.RELEASE")
+    compile("org.springframework.boot:spring-boot-starter-web:2.2.0.RELEASE") {
         exclude("spring-boot-starter-tomcat")
     }
-    compile("org.springframework.boot:spring-boot-starter-undertow:2.1.8.RELEASE")
-    compile("org.springframework:spring-context-indexer:5.1.9.RELEASE")
-    compile("org.springframework.boot:spring-boot-configuration-processor:2.1.8.RELEASE")
+    compile("org.springframework.boot:spring-boot-starter-undertow:2.2.0.RELEASE")
+    compile("org.springframework:spring-context-indexer:5.2.0.RELEASE")
+    compile("org.springframework.boot:spring-boot-configuration-processor:2.2.0.RELEASE")
 
     compile("org.apache.commons:commons-collections4:4.4")
     compile("org.apache.commons:commons-lang3:3.9")
 
     compile("com.zaxxer:HikariCP:3.4.1")
-    compile("org.flywaydb:flyway-core:6.0.4")
+    compile("org.flywaydb:flyway-core:6.0.7")
     compile("org.postgresql:postgresql:42.2.8")
 
-    testCompile("org.springframework.boot:spring-boot-starter-test:2.1.8.RELEASE")
+    testCompile("org.springframework.boot:spring-boot-starter-test:2.2.0.RELEASE")
     testCompile("com.nhaarman.mockitokotlin2:mockito-kotlin:2.2.0")
     testCompile("org.jetbrains.kotlin:kotlin-test-junit:1.3.50")
 }
