ae3ad9a0e7abe09f9d1c2329c4e018d8fd84aa0c
Oleg Andreych <kjiec4@gmail.com>
2021-02-12 20:41:52 +0500

    * Fixed data.gov.ru link; * Bumped dependencies versions; * Removed graal args from the Procfile;

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,14 +2,14 @@ import org.gradle.api.JavaVersion.VERSION_11
 
 plugins {
     application
-    kotlin("jvm") version "1.3.50"
-    id("org.springframework.boot").version("2.2.0.RELEASE")
-    id("org.jetbrains.kotlin.plugin.spring").version("1.3.50")
-    id("com.github.ben-manes.versions").version("0.27.0")
+    kotlin("jvm") version "1.4.30"
+    id("org.springframework.boot").version("2.4.2")
+    id("org.jetbrains.kotlin.plugin.spring").version("1.4.30")
+    id("com.github.ben-manes.versions").version("0.36.0")
 }
 
 application {
-    mainClassName = "org.andreych.workcalendar.MainKt"
+    mainClass.set("org.andreych.workcalendar.MainKt")
 }
 
 allprojects {
@@ -23,13 +23,12 @@ buildscript {
     }
 
     dependencies {
-        classpath("com.github.ben-manes:gradle-versions-plugin:0.27.0")
+        classpath("com.github.ben-manes:gradle-versions-plugin:0.36.0")
     }
 }
 
 repositories {
-    jcenter()
-    maven("http://repo.maven.apache.org/maven2")
+    mavenCentral()
 }
 
 java {
@@ -37,42 +36,36 @@ java {
     targetCompatibility = VERSION_11
 }
 
-kotlin {
-    sourceSets {
-        register("src/main/kotlin")
-    }
-}
-
 dependencies {
-    compile("org.jetbrains.kotlin:kotlin-stdlib:1.3.50")
-    compile("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.2")
-    compile("org.jetbrains.kotlin:kotlin-reflect:1.3.50")
-    compile("com.fasterxml.jackson.module:jackson-module-kotlin:2.10.0")
-    compile("org.mnode.ical4j:ical4j:3.0.11")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.4.30")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.2")
+    implementation("org.jetbrains.kotlin:kotlin-reflect:1.4.30")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.12.1")
+    implementation("org.mnode.ical4j:ical4j:3.0.18")
 
-    compile("org.springframework.boot:spring-boot-devtools:2.2.0.RELEASE")
-    compile("org.springframework.boot:spring-boot-starter-jdbc:2.2.0.RELEASE")
-    compile("org.springframework.boot:spring-boot-starter-web:2.2.0.RELEASE") {
+    implementation("org.springframework.boot:spring-boot-devtools:2.4.2")
+    implementation("org.springframework.boot:spring-boot-starter-jdbc:2.4.2")
+    implementation("org.springframework.boot:spring-boot-starter-web:2.4.2") {
         exclude("spring-boot-starter-tomcat")
     }
-    compile("org.springframework.boot:spring-boot-starter-undertow:2.2.0.RELEASE")
-    compile("org.springframework:spring-context-indexer:5.2.0.RELEASE")
-    compile("org.springframework.boot:spring-boot-configuration-processor:2.2.0.RELEASE")
+    implementation("org.springframework.boot:spring-boot-starter-undertow:2.4.2")
+    implementation("org.springframework:spring-context-indexer:5.3.3")
+    implementation("org.springframework.boot:spring-boot-configuration-processor:2.4.2")
 
-    compile("org.apache.commons:commons-collections4:4.4")
-    compile("org.apache.commons:commons-lang3:3.9")
+    implementation("org.apache.commons:commons-collections4:4.4")
+    implementation("org.apache.commons:commons-lang3:3.11")
 
-    compile("com.zaxxer:HikariCP:3.4.1")
-    compile("org.flywaydb:flyway-core:6.0.7")
-    compile("org.postgresql:postgresql:42.2.8")
+    implementation("com.zaxxer:HikariCP:4.0.1")
+    implementation("org.flywaydb:flyway-core:7.5.3")
+    implementation("org.postgresql:postgresql:42.2.18")
 
-    testCompile("org.springframework.boot:spring-boot-starter-test:2.2.0.RELEASE")
-    testCompile("com.nhaarman.mockitokotlin2:mockito-kotlin:2.2.0")
-    testCompile("org.jetbrains.kotlin:kotlin-test-junit:1.3.50")
+    testImplementation("org.springframework.boot:spring-boot-starter-test:2.4.2")
+    testImplementation("com.nhaarman.mockitokotlin2:mockito-kotlin:2.2.0")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:1.4.30")
 }
 
 tasks.withType(org.jetbrains.kotlin.gradle.tasks.KotlinCompile::class.java).all {
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "11"
     }
 }
--- src/main/kotlin/org/andreych/workcalendar/datasource/CalendarDataRetriever.kt
+++ src/main/kotlin/org/andreych/workcalendar/datasource/CalendarDataRetriever.kt
@@ -9,7 +9,6 @@ import org.slf4j.LoggerFactory
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.web.client.RestTemplateBuilder
 import org.springframework.web.client.RestTemplate
-import org.springframework.web.client.getForObject
 
 @ConfigurationProperties("open.data")
 class RetrieverConfiguration {
@@ -17,29 +16,30 @@ class RetrieverConfiguration {
 }
 
 class CalendarDataRetriever(
-        restTemplateBuilder: RestTemplateBuilder,
-        config: RetrieverConfiguration,
-        private val objectMapper: ObjectMapper) {
+    restTemplateBuilder: RestTemplateBuilder,
+    config: RetrieverConfiguration,
+    private val objectMapper: ObjectMapper
+) {
 
     companion object {
         private const val URL_TEMPLATE =
-                "/dataset/7708660670-proizvcalendar/version/20151123T183036/content/?access_token={access_token}"
+            "/dataset/7708660670-proizvcalendar/version/20151123T183036/content/?access_token={access_token}"
         private val LOG = LoggerFactory.getLogger(CalendarDataRetriever::class.java)
     }
 
     private val accessToken = mapOf("access_token" to config.accessToken)
 
     private val restTemplate: RestTemplate = restTemplateBuilder
-            .rootUri("http://data.gov.ru/api")
-            .build()
+        .rootUri("https://data.gov.ru/api")
+        .build()
 
     suspend fun getData(): Pair<String?, List<YearData>> {
+        LOG.info("Fetching calendar data from government service.")
         val deferred = withContext(Dispatchers.IO) {
             return@withContext async {
-                LOG.info("Fetching calendar data from government service.")
-                val response: String? = restTemplate.getForObject(URL_TEMPLATE, accessToken)
-                val parsedResponse: Array<YearData>? = objectMapper.readValue(response, Array<YearData>::class.java)
-                return@async Pair(response, parsedResponse?.asList() ?: emptyList())
+                val response: String? = restTemplate.getForObject(URL_TEMPLATE, String::class.java, accessToken)
+                val value = objectMapper.readValue(response, Array<YearData>::class.java)
+                return@async Pair(response, value?.asList() ?: emptyList())
             }
         }
         val result = deferred.await()
